8713df04a2e1644debba2c2cb20edc322f12d66b
GrishaTkach <grisatkac@gmail.com>
2017-11-05 23:39:13 +0300

    gues commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,32 @@
-class GuessingGame {
-    constructor() {}
-
+lass GuessingGame {
+    constructor() {
+    	this.guess=0;
+		this.lastmax=0;
+		this.lastmin=0;
+    }
     setRange(min, max) {
+    	this.lastmax=max;
+		this.lastmin=min;
 
     }
 
     guess() {
-
+    	if((this.lastmax-this.lastmin)%2==0)
+			{this.gues=this.lastmin+(this.lastmax-this.lastmin)/2;
+			return this.gues;}
+		else
+			{this.gues=this.lastmin+(this.lastmax-this.lastmin+1)/2;
+			return this.gues;}
     }
 
     lower() {
+    	this.lastmax=this.gues;
 
     }
 
     greater() {
+    	this.lastmin=this.gues;
 
     }
 }
-
 module.exports = GuessingGame;
